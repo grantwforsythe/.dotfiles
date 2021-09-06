5d953aa499bd8be1cea0dd14a1920b6d072da0fa
Grant Forsythe <grantwforsythe@gmail.com>
2021-09-06 03:19:47 +0000

    Fix files

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ dotfiledir=${homedir}/dotfiles
 bundle=${homedir}/.vim/bundle
 
 # list of files/folders to symlink in ${homedir}
-files="bash_profile bashrc vimrc.tmux.conf gitconfig"
+files="bash_profile bashrc vimrc .tmux.conf gitconfig"
 
 # change to the dotfiles directory
 echo "Changing to the ${dotfiledir} directory"
