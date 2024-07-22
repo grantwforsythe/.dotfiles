eb51e0a1aaac42d82958a4f116158dc23333eb37
Grant Forsythe <23422098+grantwforsythe@users.noreply.github.com>
2024-07-22 10:11:53 -0400

    feat: add powershell profile

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+Set-Alias vim nvim
+Set-Alias vi nvim
+Set-Alias g git
+
+# Set Starship as prompt
+Invoke-Expression (&starship init powershell)
+$ENV:STARSHIP_CONFIG = "$LOCALAPPDATA\starship\starship.toml"
+$ENV:STARSHIP_CACHE = "$APPDATA\Temp"
+
+Import-Module PSReadLine
+
+Set-PSReadLineOption -EditMode Vi -ViModeIndicator Cursor
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompleteion for Arrow keys
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineOption -PredictionSource History
