569548bccd5deed1b4968526afff0722bc1cb942
Grant Forsythe <23422098+grantwforsythe@users.noreply.github.com>
2024-07-22 10:10:06 -0400

    feat: add install.ps1
    Create install script for PowerShell.

---
+++ install.ps1
@@ -0,0 +1,39 @@
+$currentPath = $PWD.Path  
+
+Copy-Item -Path $currentPath\Microsoft.PowerShell_profile.ps1 -Destination $env:USERPROFILE\Documents\WindowsPowershell\Microsoft.PowerShell_profile.ps1 -Force
+
+cd $currentPath\git
+if (Test-Path -LiteralPath $env:USERPROFILE\.gitconfig) {
+  Remove-Item -LiteralPath $env:USERPROFILE\.gitconfig 
+}
+
+New-Item -Path $env:USERPROFILE\.gitconfig -ItemType SymbolicLink -Value .gitconfig
+
+cd $currentPath\alacritty\.config\
+if (Test-Path -LiteralPath $env:APPDATA\alacritty) {
+  Remove-Item -LiteralPath $env:APPDATA\alacritty -Recurse
+}
+
+New-Item -Path $env:APPDATA\alacritty -ItemType SymbolicLink -Value alacritty\
+
+cd $currentPath\lazygit\.config\
+if (Test-Path -LiteralPath $env:LOCALAPPDATA\lazygit) {
+  Remove-Item -LiteralPath $env:LOCALAPPDATA\lazygit -Recurse
+}
+
+New-Item -Path $env:LOCALAPPDATA\lazygit -ItemType SymbolicLink -Value lazygit\
+
+cd $currentPath\nvim\.config\
+if (Test-Path -LiteralPath $env:LOCALAPPDATA\nvim) {
+  Remove-Item -LiteralPath $env:LOCALAPPDATA\nvim -Recurse
+}
+
+New-Item -Path $env:LOCALAPPDATA\nvim -ItemType SymbolicLink -Value nvim
+
+cd $currentPath\starship\.config\
+if (Test-Path -LiteralPath $env:LOCALAPPDATA\starship) {
+  Remove-Item -LiteralPath $env:LOCALAPPDATA\starship -Recurse
+}
+
+New-Item -Path $env:LOCALAPPDATA -Name "starship" -ItemType Directory
+New-Item -Path $env:LOCALAPPDATA\starship\starship.toml -ItemType SymbolicLink -Value starship.toml
