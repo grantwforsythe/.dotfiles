eca6732de7bbfb248913eeaa0b05a8842d8d0c63
Grant Forsythe <grantwforsythe@gmail.com>
2025-06-02 06:53:57 -0400

    Revert "refactor: mv wezterm sessionizer to plugin"
    This reverts commit de850da0a5a685fd00e164485d144a095529943f.

--- wezterm/.config/wezterm/main/init.lua
+++ wezterm/.config/wezterm/main/init.lua
@@ -1,11 +1,8 @@
 local wezterm = require("wezterm")
 
 local config = require("main.global.config")
-local utils = require("main.global.utils")
 
-if utils.is_windows and os.getenv("USE_WSL") ~= nil then
-	config.default_domain = "WSL:Ubuntu"
-elseif utils.is_windows then
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.default_prog = { "powershell", "-NoLogo" }
 	config.launch_menu = {
 		{
@@ -17,9 +14,16 @@ end
 
 config.keys = require("main.global.remap")
 
--- To update all plugins type CTLR + Shift + L to open debug mode
--- Then run the command `wezterm.plugin.update_all()` to update all plugins
-local sessionizer = wezterm.plugin.require "https://github.com/grantwforsythe/sessionizer.wezterm"
-sessionizer.apply_to_config(config)
+-----------------------------------------------
+--                   Plugins                 --
+-----------------------------------------------
+local sessionizer = require("main.plugins.sessionizer")
+local projects = {
+	"~",
+	"~/repos",
+}
+
+sessionizer.set_projects(projects)
+sessionizer.configure(config)
 
 return config
