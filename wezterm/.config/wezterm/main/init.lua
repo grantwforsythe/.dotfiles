935a700a32b2b214e0d88bf6413d71f6cc69d86c
Grant Forsythe <grantwforsythe@gmail.com>
2025-06-12 06:59:21 -0400

    feat: add workspace to zoxide

--- wezterm/.config/wezterm/main/init.lua
+++ wezterm/.config/wezterm/main/init.lua
@@ -3,18 +3,21 @@ local wezterm = require("wezterm")
 local config = require("main.global.config")
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_prog = { "powershell", "-NoLogo" }
-	config.launch_menu = {
-		{
-			label = "PowerShell",
-			args = { "powershell.exe", "-NoLogo" },
-		},
-	}
+    config.default_prog = { "powershell", "-NoLogo" }
+    config.launch_menu = {
+        {
+            label = "PowerShell",
+            args = { "powershell.exe", "-NoLogo" },
+        },
+    }
 end
 
 config.keys = require("main.global.remap")
 
+--NOTE: Use file URL, e.g. 'file:///C:/User/...', when developing a plugin locally
 local sessionizer = wezterm.plugin.require("https://github.com/grantwforsythe/sessionizer.wezterm")
-sessionizer.apply_to_config(config, { paths = { "~\\repos" } })
+sessionizer.apply_to_config(config,
+    { paths = { "~\\repos" }, add_workspace_to_zoxide = true }
+)
 
 return config
