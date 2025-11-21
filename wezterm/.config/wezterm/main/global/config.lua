8f5d850e6e2a2f7f7de7e38400da2729a074ea34
Grant Forsythe <23422098+grantwforsythe@users.noreply.github.com>
2025-11-21 12:41:32 -0500

    fix(wezterm): action for renaming panes
    Added custom actions to the command palette for updating plugins and renaming tabs. Configured environment variables and updated color settings for the tab bar.

--- wezterm/.config/wezterm/main/global/config.lua
+++ wezterm/.config/wezterm/main/global/config.lua
@@ -1,12 +1,39 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
+wezterm.on("augment-command-palette", function(window, pane)
+	return {
+		{
+			brief = "Plugin: Update All Plugins",
+			icon = "cod_github_inverted",
+			doc = "Attempt to fast-forward each of the repos in the plugin directory.",
+			action = wezterm.action_callback(function(_window, _pane)
+				wezterm.plugin.update_all()
+				-- Reload the configuration to reflect any changes
+				wezterm.reload_configuration()
+			end),
+		},
+		{
+			brief = "Rename current tab",
+			doc = "Rename the current tab",
+			action = act.PromptInputLine({
+				description = "Enter new name for tab",
+				action = wezterm.action_callback(function(window, pane, line)
+					if line then
+						window:active_tab():set_title(line)
+					end
+				end),
+			}),
+		},
+	}
+end)
+
 local config = wezterm.config_builder()
 
 config.color_scheme = "Gruvbox dark, medium (base16)"
 
 config.hide_tab_bar_if_only_one_tab = true
-config.tab_bar_at_bottom = true
+
 config.use_fancy_tab_bar = true
 
 config.window_padding = {
@@ -20,21 +47,19 @@ config.font_size = 14.0
 
 config.window_close_confirmation = "NeverPrompt"
 
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+config.set_environment_variables = {
+	LANG = "en_US.UTF-8",
+	PATH = wezterm.executable_dir .. ";" .. os.getenv("PATH"),
+}
 
-wezterm.on("augment-command-palette", function(window, pane)
-	return {
-		{
-			brief = "Plugin: Update All Plugins",
-			icon = "cod_github_inverted",
-			doc = "Attempt to fast-forward each of the repos in the plugin directory.",
-			action = wezterm.action_callback(function(_window, _pane)
-				wezterm.plugin.update_all()
-				-- Reload the configuration to reflect any changes
-				wezterm.reload_configuration()
-			end),
-		},
-	}
-end)
+config.colors = {
+	tab_bar = {
+		new_tab = { bg_color = "#121212", fg_color = "#FCE8C3", intensity = "Bold" },
+		new_tab_hover = { bg_color = "#121212", fg_color = "#FBB829", intensity = "Bold" },
+		active_tab = { bg_color = "#121212", fg_color = "#FCE8C3" },
+	},
+}
+
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
 
 return config
